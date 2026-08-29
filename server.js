e9c4b00de9ef5f3c82d14ffffdfa69e4c868156b
Atharsh Sathiyan <atharsh100@gmail.com>
2026-08-29 18:08:12 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
 dotenv.config();
+const connectDB = require('./config/db');
 connectDB();
 
 const app = express();
@@ -32,5 +32,5 @@ app.use('/api/admin', require('./routes/adminRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT} Use http://localhost:5000`);
 });
